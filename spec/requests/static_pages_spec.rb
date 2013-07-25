a386ce0b3c8387f7af863d3626802ab66bac18c7
Scott Van Essen <scott.vanessen@gmail.com>
2013-07-25 16:56:16 -0700

    Problem 1, have_selector method

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,7 +27,7 @@ describe "Static pages" do
   describe "Contact Page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
 
